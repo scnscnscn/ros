f3438539ce66f440162dd297a473206646bff5fb
scnscn <727265958@qq.com>
2025-05-09 21:01:08 +0800

    param cpp

--- src/param/src/param_delete.cpp
+++ src/param/src/param_delete.cpp
@@ -16,12 +16,12 @@ int main(int argc, char *argv[])
     bool exists_after1 = nh.hasParam("nh_int");
     ROS_INFO("第一次删除后 nh_int 是否存在: %d", exists_after1);
 
-    // 2. 再次尝试删除同一个参数 nh_int
-    bool r2 = ros::param::del("nh_int");
-    ROS_INFO("第二次 ros::param::del(\"nh_int\") 结果: %d", r2);
+    // 2. 再次尝试删除 param_int 参数
+    bool r2 = ros::param::del("param_int");
+    ROS_INFO("第二次 ros::param::del(\"param_int\") 结果: %d", r2);
 
-    bool exists_after2 = nh.hasParam("nh_int");
-    ROS_INFO("第二次删除后 nh_int 是否存在: %d", exists_after2);
+    bool exists_after2 = nh.hasParam("param_int");
+    ROS_INFO("第二次删除后 param_int 是否存在: %d", exists_after2);
 
     // 3. 检查并删除 param_int 参数（与上面的 nh_int 是不同的参数）
     bool exists_before3 = nh.hasParam("param_int");
