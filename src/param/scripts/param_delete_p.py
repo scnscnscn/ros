8ca9e9a41de30dbab08684bb8600fdddba25ffb0
scnscn <727265958@qq.com>
2025-05-09 21:24:46 +0800

    param python

---
+++ src/param/scripts/param_delete_p.py
@@ -0,0 +1,9 @@
+import rospy
+
+if __name__ == "__main__":
+    rospy.init_node("delete_param_p")
+
+    try:
+        rospy.delete_param("p_int")
+    except Exception as e:
+        rospy.loginfo("删除失败")
